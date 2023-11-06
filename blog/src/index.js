c7199e6b3eb86e60c1297b58b5e15e59c63cbed1
taed13 <taedtech13@gmail.com>
2023-11-06 21:40:45 +0700

    up to date done session 2

--- blog/src/index.js
+++ blog/src/index.js
@@ -6,9 +6,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // template engine
 app.engine(
@@ -26,6 +28,16 @@ app.get("/", (req, res) => {
 
 app.get("/news", (req, res) => {
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.q);
+  res.render("search");
 });
 
 app.listen(port, () => {
